059e66a400317d79ebc5a7b9030e367a4d2f15fa
Ariotee <mat_990011@mail.ru>
2024-09-24 18:45:08 +0700

    add/added extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'accent-color': '#10B981',
+				'logo-color': '#1b1c57',
+				'first-white-color': '#F0F3FD',
+				'border-page-color': 'rgba(255, 255, 255, 0.3)',
+				'green-first': '#D1FAE5',
+			},
+			fontFamily: {
+				'main-ff': ['"Lexend", sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 }
-
